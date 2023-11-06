bbb9e661c2c387354b732006dadf43760cc4c57c
Prachi Gupta <117148255+prachiguptadev@users.noreply.github.com>
2023-11-06 15:58:14 +0000

    create a aaray with 5 object

---
+++ index.js
@@ -0,0 +1,67 @@
+const fruitArray = [
+  {
+    name: "Apple",
+    price: 20,
+    isPaid: true,
+  },
+  {
+    name: "Mango",
+    price: 30,
+    isPaid: true,
+  },
+  {
+    name: "PineApple",
+    price: 70,
+    isPaid: true,
+  },
+  {
+    name: "Papaya",
+    price: 50,
+    isPaid: true,
+  },
+  {
+    name: "WaterMalen",
+    price: 100,
+    isPaid: true,
+  },
+];
+
+const firstFruitName = fruitArray[0].name;
+const firstFruitPrice = fruitArray[0].price;
+const firstFruitIsPaid = fruitArray[0].isPaid;
+
+console.log(
+  `${firstFruitName}, This is worth ${firstFruitPrice} rupees. Is it paid? ${firstFruitIsPaid}`
+);
+
+const secondFruitName = fruitArray[1].name;
+const secondFruitPrice = fruitArray[1].price;
+const secondFruitIsPaid = fruitArray[1].isPaid;
+
+console.log(
+  `${secondFruitName}, This is worth ${secondFruitPrice} rupees. Is it paid? ${secondFruitIsPaid}`
+);
+
+const thirdFruitName = fruitArray[2].name;
+const thirdFruitPrice = fruitArray[2].price;
+const thirdFruitIsPaid = fruitArray[2].isPaid;
+
+console.log(
+  `${thirdFruitName}, This is worth ${thirdFruitPrice} rupees. Is it paid? ${thirdFruitIsPaid}`
+);
+
+const fourthFruitName = fruitArray[3].name;
+const fourthFruitPrice = fruitArray[3].price;
+const fourthFruitIsPaid = fruitArray[3].isPaid;
+
+console.log(
+  `${fourthFruitName}, This is worth ${fourthFruitPrice} rupees. Is it paid? ${fourthFruitIsPaid}`
+);
+
+const fivthFruitName = fruitArray[4].name;
+const fivthFruitPrice = fruitArray[4].price;
+const fivthFruitIsPaid = fruitArray[4].isPaid;
+
+console.log(
+  `${fivthFruitName}, This is worth ${fivthFruitPrice} rupees. Is it paid? ${fivthFruitIsPaid}`
+);
